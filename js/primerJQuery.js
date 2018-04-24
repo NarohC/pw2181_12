d69b2eb8f91449ac58793d05766ffa116bd9b0ba
CCDM24 <ccdm24@CCDM24.local>
2018-04-24 12:54:52 -0600

    Primer JQuery

---
+++ js/primerJQuery.js
@@ -0,0 +1,18 @@
+var inicia = function(){
+	var nuevo =function(){
+		$.ajax({
+  			url: 'https://randomuser.me/api/',
+  			dataType: 'json',
+  			success: function(data) {
+    			$("#nombre").html(data.results[0].name.first +" "+ data.results[0].name.last);
+    			$("#foto").attr("src",data.results[0].picture.large);
+  			}
+		});
+	}
+	//alert("vagina lista");
+	$("#btnNuevo").on("click",nuevo);
+	// $("#btNuevo").off("click",nuevo);
+	
+}
+
+$(document).ready(inicia);
