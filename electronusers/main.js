f681104583821a209a0c5c715f6ded4685ccde25
CCDM24 <ccdm24@CCDM24.local>
2018-04-30 13:03:02 -0600

    Electron

---
+++ electronusers/main.js
@@ -0,0 +1,24 @@
+//Aplicacioó general
+const app = require('electron').app;
+//Uso de las pantallas de sistema
+const BrowserWindow=require('electron').BrowserWindow;
+//Ruta de la carpeta base
+const path=require('path');
+//URL de las paginas
+const url=require('url');
+
+//ECMASCRIPT 6 - JS
+let PantallaPrincipal;
+
+function muestraPantallaPrincipal() {
+	PantallaPrincipal= new BrowserWindow({width:628,height:825});
+	PantallaPrincipal.loadURL(url.format({
+		//join: concatenar cadenas
+		pathname:path.join(__dirname,'index.html'),
+		protocol: 'file',
+		slashed: true
+	}));
+	PantallaPrincipal.show();
+}
+
+app.on('ready',muestraPantallaPrincipal);
