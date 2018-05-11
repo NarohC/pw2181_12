acf7dc0dbd369dc412ccd79308072b4c5b52f760
CCDM24 <ccdm24@CCDM24.local>
2018-05-11 12:58:28 -0600

    Listado de Usuarios Segunda Ventana

--- listadousuarios/index.js
+++ listadousuarios/index.js
@@ -7,7 +7,21 @@ const url = require ('url')
 let pantallaDetalle;
 var Usuarios = new Array(20);
 
+function datos(nombre,genero,foto,direccion,telefono){
+	this.nombre=nombre;
+	this.genero=genero;
+	this.foto= foto;
+	this.direccion= direccion;
+	this.telefono= telefono;
+}
 function inicia(){
+	var resultado = "";
+	var nombre = "";
+	var foto = "";
+	var genero = "";
+	var direccion = "";
+	var telefono = "";
+
 	$.ajax({
 		url: 'https://randomuser.me/api/?results=20',
 		dataType: 'json',
@@ -15,8 +29,12 @@ function inicia(){
 			for (var i = 0; i < 20; i++) {
 				nombre=data.results[i].name.first+" "+data.results[i].name.last;
 				foto=data.results[i].picture.medium;
+				genero=data.results[i].gender;
+				direccion=data.results[i].location.street;
+				telefono=data.results[i].phone;
 				resultado="<li><img src="+foto+">"+nombre+"<button id="+i+">Detalle</button>"
 				$("#lstUsuarios").append(resultado);
+				Usuarios[i] = new datos(nombre,genero,foto,direccion,telefono);
 			}
 		}
 	});
@@ -24,6 +42,11 @@ function inicia(){
 
 function botonDetalle(){
 	//alert(this.id);
+	// alert(Usuarios[this.id].nombre);
+	pantallaDetalle= new BrowserWindow({width:320,height:425});
+	pantallaDetalle/loadURL(url.format({
+		pathname: path.join(__dirname, 'datalleusuarios.html')
+	}))
 
 }
 
